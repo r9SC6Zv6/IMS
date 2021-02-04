711be52e9d6c468469ed2cfd98b0b3afcf7023fd
Ervin Musincki <emusincki@academytrainee.com>
2021-02-04 10:51:04 +0000

    added exception handling and updated sql-schema

---
+++ src/main/java/com/qa/ims/exceptions/ItemNotFoundException.java
@@ -0,0 +1,12 @@
+package com.qa.ims.exceptions;
+
+public class ItemNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1319030538360418254L;
+
+	
+	
+}
