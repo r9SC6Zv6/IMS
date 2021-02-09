002900a2e1e18a6eecd8295ce2040cc0233fd883
Ervin Musincki <emusincki@academytrainee.com>
2021-02-09 13:55:55 +0000

    updated sql test files with item and order tables

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,35 @@
+DROP ALL OBJECTS [DELETE FILES];
+DROP TABLE IF EXISTS `order_item`;
+DROP TABLE IF EXISTS `orders`;
 DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `items`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `items` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) DEFAULT NULL,
+    `value` FLOAT DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`customer_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `order_item` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`order_id` INT NOT NULL,
+	`item_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`),
+	FOREIGN KEY (`item_id`) REFERENCES `items`(`id`)
 );
